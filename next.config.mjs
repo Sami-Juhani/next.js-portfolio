925b30ff1baf6adb7d470e5f4c18a925fb407ba9
Sami-Juhani <sami.paananen@gmail.com>
2024-06-08 14:00:33 +0300

    settings for static deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* SETTINGS FOR STATIC DEPLOYMENT, DELETE ON FINAL PRODUCT */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "samipaan.com",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
