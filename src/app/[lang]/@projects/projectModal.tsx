30535f4be50d6998e0e287659a953a4aa76273c3
Sami-Juhani <sami.paananen@gmail.com>
2024-06-26 13:49:54 +0300

    Add eventlistener to close modal if user clicks outside

--- src/app/[lang]/@projects/projectModal.tsx
+++ src/app/[lang]/@projects/projectModal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Dispatch, ReactNode, SetStateAction } from "react"
+import { Dispatch, ReactNode, SetStateAction, useEffect, useRef } from "react"
 import styles from "./projects.module.css"
 import useIcons from "@/hooks/useIcons"
 
@@ -12,11 +12,24 @@ type ProjectModalProps = {
 
 export function ProjectModal({ setIsOpen, Component, setActiveComponent }: ProjectModalProps) {
   const { CloseIcon } = useIcons().action
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+
+    return () => document.removeEventListener("mousedown", handleClickOutside)
+  }, [setIsOpen])
 
   return (
     <>
       <div className={"overlay"}></div>
-      <div className={styles.__projectModal}>
+      <div className={styles.__projectModal} ref={modalRef}>
         <CloseIcon
           className={styles.closeIcon}
           onClick={() => {
