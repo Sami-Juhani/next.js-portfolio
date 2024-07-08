b8dab4aa72fb7671ece0adb4d3580745c43aee86
Sami-Juhani <sami.paananen@gmail.com>
2024-07-08 15:41:35 +0300

    Add new font specialElite

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,3 +1,4 @@
-import { Mr_Dafoe } from "next/font/google"
+import { Mr_Dafoe, Special_Elite } from "next/font/google"
 
 export const dafoe = Mr_Dafoe({ weight: "400", subsets: ["latin"] })
+export const specialElite = Special_Elite({ weight: "400", subsets: ["latin"] })
