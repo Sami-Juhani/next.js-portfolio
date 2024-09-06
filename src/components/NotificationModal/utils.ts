a90cc7dca017f951f66fe59b53f93c4134039728
Sami-Juhani <sami.paananen@gmail.com>
2024-09-06 23:23:01 +0300

    (fix): Add autoDismiss

--- src/components/NotificationModal/utils.ts
+++ src/components/NotificationModal/utils.ts
@@ -3,7 +3,7 @@ import { NotificationModalOptions } from "./NotificationModal";
 export const DEFAULT_MODAL_OPTIONS: NotificationModalOptions = {
   blurBg: false,
   position: "topCenter",
-  autoDismiss: false,
-  /*autoDismissTimeout: 5000,*/
+  autoDismiss: true,
+  autoDismissTimeout: 5000,
   size: "sm",
-};
+}
