7e5292a9fac6585508ec0fa5fd9071e8b6235f8b
Sami-Juhani <sami.paananen@gmail.com>
2024-09-11 22:04:04 +0300

    (dev): Add use server on action. Add deleteUser()

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,4 +1,7 @@
+"use server"
+
 import prisma from "../lib/db"
+import { updateBlogLikes } from "./blogs"
 
 export async function getOrCreateUser(email: string) {
   const user = await prisma.user.findFirst({
@@ -68,4 +71,38 @@ export async function removeBlogLike(email: string, blogId: string) {
   })
 
   return updatedUser
+}
+
+export type DeleteUserPayload = {
+  formData: FormData
+  userId: string | undefined
+  validConfirmation: string
+}
+
+export type DeleteUserResult = { completed: boolean; error?: string } | undefined
+
+export async function deleteUser(prevState: unknown, payload: DeleteUserPayload): Promise<DeleteUserResult> {
+  if (payload.formData.get("confirmation") !== payload.validConfirmation) {
+    return { completed: false, error: "Confirmation is not valid" }
+  }
+
+  if (payload.userId == undefined) return { completed: false, error: "User Id was not provided" }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: payload.userId,
+    },
+  })
+
+  if (!user) return { completed: false, error: "User was not found" }
+
+  user.blogLikes.map((id) => updateBlogLikes(id, false))
+
+  await prisma.user.delete({
+    where: {
+      id: payload.userId,
+    },
+  })
+
+  return { completed: true, error: "" }
 }
